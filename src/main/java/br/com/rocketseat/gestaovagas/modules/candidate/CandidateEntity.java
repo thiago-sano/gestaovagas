9e093ec6cfd1b2cd79a1765125eaed0dc8a22bb7
thiago.sano <thiago.sano@outlook.com>
2025-04-01 16:43:03 -0300

    feat: validation and validation message for username, email and password

--- src/main/java/br/com/rocketseat/gestaovagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/rocketseat/gestaovagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,9 @@
 package br.com.rocketseat.gestaovagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,8 +12,14 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^[^\s]+$", message = "O campo [username] não deve conter espaço")
     private String username;
+
+    @Email(message = "O campo [e-mail] deve conter um e-mail válido")
     private String email;
+
+    @Length(min = 10, max = 100, message = "O campo [senha] deve conter entre 10 e 100 caracteres")
     private String password;
     private String description;
     private String curriculum;
--- src/main/java/br/com/rocketseat/gestaovagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/rocketseat/gestaovagas/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,7 @@
 package br.com.rocketseat.gestaovagas.modules.candidate.controllers;
 
 import br.com.rocketseat.gestaovagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ public class CandidateController {
 
 
     @PostMapping
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
     }
