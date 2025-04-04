f23f8e1e98f35fb7e9546a6cdc896dcd3f2a9cf8
thiago.sano <thiago.sano@outlook.com>
2025-04-04 09:10:39 -0300

    feat: created CompanyEntity

---
+++ src/main/java/br/com/rocketseat/gestaovagas/modules/company/entity/CompanyEntity.java
@@ -0,0 +1,39 @@
+package br.com.rocketseat.gestaovagas.modules.company.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Pattern(regexp = "^[^\s]+$", message = "O campo [username] não deve conter espaço")
+    private String username;
+
+    @Email(message = "O campo [e-mail] deve conter um e-mail válido")
+    private String email;
+
+    @Length(min = 10, max = 100, message = "O campo [senha] deve conter entre 10 e 100 caracteres")
+    private String password;
+    private String website;
+    private String name;
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
