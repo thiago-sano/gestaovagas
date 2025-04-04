cab4e2fec4e1a1db1a303797b651e4aa5225a86c
thiago.sano <thiago.sano@outlook.com>
2025-04-04 11:28:23 -0300

    feat: created JobController

---
+++ src/main/java/br/com/rocketseat/gestaovagas/modules/company/repositories/JobRespository.java
@@ -0,0 +1,9 @@
+package br.com.rocketseat.gestaovagas.modules.company.repositories;
+
+import br.com.rocketseat.gestaovagas.modules.company.entities.JobEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface JobRespository extends JpaRepository<JobEntity, UUID> {
+}
---
+++ src/main/java/br/com/rocketseat/gestaovagas/modules/company/useCases/CreateJobUseCase.java
@@ -0,0 +1,17 @@
+package br.com.rocketseat.gestaovagas.modules.company.useCases;
+
+import br.com.rocketseat.gestaovagas.modules.company.entities.JobEntity;
+import br.com.rocketseat.gestaovagas.modules.company.repositories.JobRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateJobUseCase {
+
+    @Autowired
+    private JobRespository jobRespository;
+
+    public JobEntity execute(JobEntity jobEntity) {
+        return this.jobRespository.save(jobEntity);
+    }
+}
