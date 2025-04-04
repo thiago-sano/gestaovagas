cc4cc85e1475dfef38c95559350e74959ce2e118
thiago.sano <thiago.sano@outlook.com>
2025-04-04 10:20:13 -0300

    feat: created CreateCompanyUseCase

---
+++ src/main/java/br/com/rocketseat/gestaovagas/modules/company/repositories/CompanyRepository.java
@@ -0,0 +1,12 @@
+package br.com.rocketseat.gestaovagas.modules.company.repositories;
+
+import br.com.rocketseat.gestaovagas.modules.company.entities.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
+
+    Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+}
