d932c5d61590e0da30c7ebf7b9ab0e88a61b39c9
thiago.sano <thiago.sano@outlook.com>
2025-04-04 09:51:26 -0300

    feat: created JobEntity

--- src/main/java/br/com/rocketseat/gestaovagas/modules/company/entities/CompanyEntity.java
+++ src/main/java/br/com/rocketseat/gestaovagas/modules/company/entities/CompanyEntity.java
@@ -1,11 +1,10 @@
-package br.com.rocketseat.gestaovagas.modules.company.entity;
+package br.com.rocketseat.gestaovagas.modules.company.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
