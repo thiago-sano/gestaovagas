7fd2a3f38c96ae5162627fcbdf2fed59e01ddeab
thiago.sano <thiago.sano@outlook.com>
2025-04-09 10:51:25 -0300

    feat: refatorando JobController

--- src/main/java/br/com/rocketseat/gestaovagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/rocketseat/gestaovagas/modules/company/controllers/JobController.java
@@ -1,13 +1,18 @@
 package br.com.rocketseat.gestaovagas.modules.company.controllers;
 
+import br.com.rocketseat.gestaovagas.modules.company.dto.CreateJobDTO;
 import br.com.rocketseat.gestaovagas.modules.company.entities.JobEntity;
 import br.com.rocketseat.gestaovagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/job")
@@ -17,7 +22,16 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping
-    public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
+    public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+        var companyId = request.getAttribute("company_id");
+
+        var jobEntity = JobEntity.builder()
+                .companyId(UUID.fromString(companyId.toString()))
+                .benefits(createJobDTO.getBenefits())
+                .description(createJobDTO.getDescription())
+                .level(createJobDTO.getLevel())
+                .build();
+
         return this.createJobUseCase.execute(jobEntity);
     }
 }
