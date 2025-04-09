5d43fd04190137569aa39fa34cac8593beb3f937
thiago.sano <thiago.sano@outlook.com>
2025-04-09 11:48:27 -0300

    feat: hash da senha do candidato

--- src/main/java/br/com/rocketseat/gestaovagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/rocketseat/gestaovagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import br.com.rocketseat.gestaovagas.exceptions.UserFoundException;
 import br.com.rocketseat.gestaovagas.modules.candidate.entities.CandidateEntity;
 import br.com.rocketseat.gestaovagas.modules.candidate.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,19 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository
                 .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
+
         return this.candidateRepository.save(candidateEntity);
     }
 }
