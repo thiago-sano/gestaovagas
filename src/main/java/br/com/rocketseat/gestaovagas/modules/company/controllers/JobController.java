9ce4b5025c15eaf1b0b8a03d6a8679087f658700
thiago.sano <thiago.sano@outlook.com>
2025-06-18 22:57:42 -0300

    chore: removed unused imports across multiple classes

--- src/main/java/br/com/rocketseat/gestaovagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/rocketseat/gestaovagas/modules/company/controllers/JobController.java
@@ -4,7 +4,6 @@ import br.com.rocketseat.gestaovagas.modules.company.dto.CreateJobDTO;
 import br.com.rocketseat.gestaovagas.modules.company.entities.JobEntity;
 import br.com.rocketseat.gestaovagas.modules.company.useCases.CreateJobUseCase;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -12,13 +11,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
--- src/main/java/br/com/rocketseat/gestaovagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/rocketseat/gestaovagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 import javax.naming.AuthenticationException;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
