95978e73d6345c4e1c613f2d91179752dcd0ac05
thiago.sano <thiago.sano@outlook.com>
2025-06-18 21:00:37 -0300

    feat: inserted HTTP requests example

--- src/main/java/br/com/rocketseat/gestaovagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/rocketseat/gestaovagas/modules/company/dto/CreateJobDTO.java
@@ -1,11 +1,17 @@
 package br.com.rocketseat.gestaovagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
 
+    @Schema(example = "Vaga para pessoa desenvolvedora júnior", requiredMode = Schema.RequiredMode.REQUIRED)
     private String description;
+
+    @Schema(example = "Gympass, Plano de saúde, Vale Alimentação e Refeição", requiredMode = Schema.RequiredMode.REQUIRED)
     private String benefits;
+
+    @Schema(example = "JÚNIOR", requiredMode = Schema.RequiredMode.REQUIRED)
     private String level;
 }
