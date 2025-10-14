ea02e38d6dfc71e9f06a0e8c2ca1c48e7700e504
thiago.sano <thiago.sano@outlook.com>
2025-10-13 23:55:54 -0300

    feat: created ApplyJobCandidateUseCaseTest for JobNotFoundException

--- src/test/java/br/com/rocketseat/gestaovagas/modules/candidate/usecases/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/br/com/rocketseat/gestaovagas/modules/candidate/usecases/ApplyJobCandidateUseCaseTest.java
@@ -1,6 +1,8 @@
 package br.com.rocketseat.gestaovagas.modules.candidate.usecases;
 
+import br.com.rocketseat.gestaovagas.exceptions.JobNotFoundException;
 import br.com.rocketseat.gestaovagas.exceptions.UserNotFoundException;
+import br.com.rocketseat.gestaovagas.modules.candidate.entities.CandidateEntity;
 import br.com.rocketseat.gestaovagas.modules.candidate.repositories.CandidateRepository;
 import br.com.rocketseat.gestaovagas.modules.company.repositories.JobRespository;
 import org.junit.jupiter.api.DisplayName;
@@ -10,7 +12,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ApplyJobCandidateUseCaseTest {
@@ -30,5 +36,15 @@ class ApplyJobCandidateUseCaseTest {
     @DisplayName("Should not be able to apply for a job with candidate not found")
     void should_not_be_able_to_apply_for_a_job_with_candidate_not_found() {
         assertThrows(UserNotFoundException.class, () -> applyJobCandidateUseCase.execute(null, null));
+    }
+
+    @Test
+    void should_not_be_able_to_apply_job_with_job_not_found() {
+        UUID candidateId = UUID.randomUUID();
+        CandidateEntity candidateEntity = new CandidateEntity();
+        candidateEntity.setId(candidateId);
+
+        when(candidateRepository.findById(candidateId)).thenReturn(Optional.of(candidateEntity));
+        assertThrows(JobNotFoundException.class, () -> applyJobCandidateUseCase.execute(candidateId, null));
     }
 }
